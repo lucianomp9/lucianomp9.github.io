2e31f5fa7d03066f5327014bee4254f7bfa1f5de
lucianomp9 <lucianoperez12@gmail.com>
2023-02-16 02:36:41 -0300

    Added new files

--- main.js
+++ main.js
@@ -1,48 +1,42 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  const header = document.querySelector("header");
 
-    const header = document.querySelector("header");
-    
-    window.addEventListener ("scroll", function() {
-        header.classList.toggle ("sticky", window.scrollY > 0);
-    });
+  window.addEventListener("scroll", function () {
+    header.classList.toggle("sticky", window.scrollY > 0);
+  });
 
+  const menuHamburger = document.querySelector(".menu-hamburger");
+  const navLinks = document.querySelector(".navbar");
 
-    new TypeIt(".typeIt", {
-        strings: [
-            "Welcome to my Portfolio!",
-            "¡Bienvenido a mi Portfolio!",
-            "私のポートフォリオへようこそ！",
-            "Willkommen in meinem Portfolio!"],
-        speed: 90,
-        deleteSpeed: null,
-        lifeLike: true,
-        cursor: true,
-        breakLines: false,
-        loop: true,
-      }).go();
-      
+  menuHamburger.addEventListener("click", () => {
+    navLinks.classList.toggle("mobile-menu");
+  });
 
-    
-      window.addEventListener('scroll', reveal);
+  new TypeIt(".typeIt", {
+    strings: ["Welcome to my Portfolio!", "¡Bienvenido a mi Portafolio!"],
+    speed: 90,
+    deleteSpeed: null,
+    lifeLike: true,
+    cursor: true,
+    breakLines: false,
+    loop: true,
+  }).go();
 
-      function reveal(){
-          var reveals = document.querySelectorAll('.reveal');
-      
-          for(var i=0; i < reveals.length; i++){
-      
-              var windowheight = window.innerHeight;
-              var revealtop = reveals[i].getBoundingClientRect().top;
-              var revealpoint= 150;
-      
-              if(revealtop < windowheight - revealpoint){
-                  reveals[i].classList.add('active');
-              }
-              else{
-                  reveals[i].classList.remove('active');
-              }
-          }
-      };
+  window.addEventListener("scroll", reveal);
 
+  function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
 
+    for (var i = 0; i < reveals.length; i++) {
+      var windowheight = window.innerHeight;
+      var revealtop = reveals[i].getBoundingClientRect().top;
+      var revealpoint = 150;
 
-})
+      if (revealtop < windowheight - revealpoint) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+});
